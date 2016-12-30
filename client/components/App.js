a1ea4fb439ed149bcb838274a1f086d2a1c02e04
Ben Bruscella <benbruscella@gmail.com>
2016-12-30 18:15:30 +1100

    Hot reloading setup with middleware

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Greetings from './Greetings'
 
-export default () => {
+class App extends React.Component {
+  render() {
     return (
-        <h1>Hello from React</h1>
+        <Greetings />
     );
+  }
 }
+
+export default App;
